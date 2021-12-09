a1d4e25b6b3279a8d820e4564b1092ad375fa2fe
Erwin James B. Manugas <james@TCP.local>
2021-12-09 10:46:56 +0800

    ticket page

--- dashboard/ship/resources/templates/_dashboard_ticket.php
+++ dashboard/ship/resources/templates/_dashboard_ticket.php
@@ -3,133 +3,133 @@
     <span id="res-message"></span>
 </div>
 <div class="row">
-    <div class="col-12">
+    <div class="col-4">
         <div class="card">
             <header class="card-header">
                 <p class="card-header-title mb-0">
-                    <span class="icon"><i class="mdi mdi-account-circle"></i></span> Create Account For Staff
+                    <span class="icon"><i class="mdi mdi-account-multiple"></i></span> Add Ticket
                 </p>
             </header>
             <div class="card-content">
-                <form id="rl_create_acc_form">
+                <form id="add_accomm_form">
                     <div class="field">
-                        <label class="label"></label>
-                        <div class="field-body d-none">
+                        <label class="label">Add Ticket</label>
+                        <!-- <div class="field-body">
                             <div class="field">
                                 <div class="control">
-                                    <input type="text" name="ship_comp" value="<?php echo $_SESSION['ship_name'];?>" class="input" readonly>
+                                    <input type="hidden" name="ship" value="<?php echo $_SESSION['stff_ship_reside'];?>">
+                                    <input type="text" autocomplete="on" name="accomodation_name" id="accomodation_name" class="input" required>
                                 </div>
-                                <p class="help"></p>
+                                <p class="help">Required. Accomodation name</p>
+                            </div>
+                        </div> -->
+                    </div>
+                    <div class="field">
+                        <label class="label">Quantity</label>
+                        <div class="field-body">
+                            <div class="field">
+                                <div class="control">
+                                    <select name="accomm_aircon" id="accomm_aircon" class="form-control">
+                                        <option selected value="">--</option>
+                                        <option value="YES">YES</option>
+                                        <option value="NO">NO</option>
+                                    </select>
+                                </div>
+                                <p class="help">Required. Aircon</p>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-6">
+                    <div class="field">
+                        <label class="label">Status</label>
+                        <div class="field-body">
                             <div class="field">
-                                <label class="label">Name</label>
-                                <div class="field-body">
-                                    <div class="field">
-                                        <div class="control">
-                                            <input type="text" name="rl_name" id="rl_name" class="input" required>
-                                        </div>
-                                        <p class="help">Required. Your name</p>
-                                    </div>
+                                <div class="control">
+                                    <input type="text" autocomplete="on" name="accomm_seat_typ" id="accomm_seat_typ" class="input" required>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="field">
-                                <label class="label">E-mail</label>
-                                <div class="field-body">
-                                    <div class="field">
-                                        <div class="control">
-                                            <input type="email" name="rl_email" id="rl_email" class="input" required>
-                                        </div>
-                                        <p class="help">Required. Your e-mail</p>
-                                    </div>
-                                </div>
+                                <p class="help">Required. Seat Type</p>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-6">
+                 
+                    <div class="field">
+                        <label class="label">Promo</label>
+                        <div class="field-body">
                             <div class="field">
-                                <label class="label">Username</label>
                                 <div class="control">
-                                    <input type="text" name="rl_username" id="rl_username" class="input" required>
+                                    <input type="text" autocomplete="on" name="accomm_typ_price" id="accomm_typ_price" value="" class="input" required>
                                 </div>
-                                <p class="help">Required. Username</p>
+                                <p class="help">Required. Price</p>
                             </div>
                         </div>
-                        <div class="col-6">
+                    </div>
+                    <div class="field">
+                        <label class="label">Discount</label>
+                        <div class="field-body">
                             <div class="field">
-                                <label class="label">Password</label>
                                 <div class="control">
-                                    <input type="password" name="rl_password" id="rl_password" class="input" required>
+                                    <input type="text" autocomplete="on" name="accomm_seat_typ" id="accomm_seat_typ" class="input" required>
                                 </div>
-                                <p class="help">Required. New password</p>
+                                <p class="help">Required. Seat Type</p>
                             </div>
                         </div>
                     </div>
                     <hr>
                     <div class="field">
                         <div class="control">
-                            <input type="submit" name="rl_btn_sbmt" id="rl_btn_sbmt" value="Submit" class="button green">
+                            <input type="submit" name="accomm_typ_btn" id="accomm_typ_btn" value="Submit" class="button green">
                         </div>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-12">
-        <div class="card">
+    <div class="col-8">
+        <div class="card has-table">
             <header class="card-header">
                 <p class="card-header-title mb-0">
-                    <span class="icon"><i class="mdi mdi-account"></i></span> Staff
+                    <span class="icon"><i class="mdi mdi-account-multiple"></i></span> Details
                 </p>
             </header>
-            <div class="card-content p-0">
-                <div class="card has-table border-0">
-                    <div class="card-content" id="all_staff_data">
-                        <img class="text-center" src="./resources/img/loading.gif" alt="Loading" style="text-align:center;width:48px;height:48px;">
-                    </div>
-                </div>
+            <div id="ship_accomm_data" class="card-content">
+                <img class="text-center" src="./resources/img/loading.gif" alt="Loading" style="text-align:center;width:48px;height:48px;">
             </div>
         </div>
     </div>
 </div>
-<div class="text-left modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="port_locationLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form id="edit_accom_form">
+            <div class="form-group">
+                <label for="edit_accom_name">Accomodation Name</label>
+                <input type="text" class="form-control form-control-sm" name="edit_accom_name" id="edit_accom_name" required>
             </div>
-            <div class="modal-body">
-                <form id="assign_role_edit_form">
-                    <div class="form-group">
-                        <label for="edit_role_name">Name</label>
-                        <input type="text" name="edit_role_name" class="text-center form-control form-control-sm" id="edit_role_name" placeholder="" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="edit_role_psswd">Email</label>
-                        <input type="email" name="edit_role_email" class="text-center form-control form-control-sm" id="edit_role_email" placeholder="" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="edit_role_uname">Username</label>
-                        <input type="text" name="edit_role_uname" class="text-center form-control form-control-sm" id="edit_role_uname" placeholder="" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="edit_role_psswd">Password</label>
-                        <input type="password" name="edit_role_psswd" class="text-center form-control form-control-sm" id="edit_role_psswd" placeholder="" required>
-                    </div>
-                    <button type="submit" id="edit_role_submit" class="btn btn-primary btn-sm form-control">Update</button>
-                </form>
+            <div class="form-group">
+                <label for="edit_accom_st">Seat Type</label>
+                <input type="text" class="form-control form-control-sm" name="edit_accom_st" id="edit_accom_st" required>
             </div>
-        </div>
+            <div class="form-group">
+                <label for="edit_accom_aircon">Aircon</label>
+                <select name="edit_accom_aircon" id="edit_accom_aircon" class="form-control-sm form-control">
+                    <option value="YES">YES</option>
+                    <option value="NO">NO</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="edit_accom_price">Price</label>
+                <input type="text" class="form-control form-control-sm" name="edit_accom_price" id="edit_accom_price" required>
+            </div>
+            <button type="submit" id="edit_accom_btn" class="btn btn-primary btn-sm form-control">Update</button>
+        </form>
+      </div>
     </div>
+  </div>
 </div>

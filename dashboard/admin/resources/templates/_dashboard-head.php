ee362abd235fc6bfc1bd7367ae4290871d5386ac
Erwin James B. Manugas <james@TCP.local>
2022-01-04 09:50:32 +0800

    admin side

--- dashboard/admin/resources/templates/_dashboard-head.php
+++ dashboard/admin/resources/templates/_dashboard-head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Dashboard - <?php echo $_SESSION['ship_name']; ?></title>
+<title>Dashboard - <?php echo $_SESSION['username']; ?></title>
 <link rel="stylesheet" href="resources/css/main.css?version=barkomatic">
 <link rel="stylesheet" href="resources/css/bootstrap.min.css">
 <link rel="icon" type="image/png" sizes="32x32" href="resources/img/favicon.png" />

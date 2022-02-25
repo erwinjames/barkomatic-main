6d0a7fc164a2831611e8353e667216e2e10fdb0b
Erwin James B. Manugas <james@TCP.local>
2022-02-25 17:25:25 +0800

    ticket editing

--- generate_ticket.php
+++ generate_ticket.php
@@ -2,56 +2,49 @@
 require_once"modules/config.php";
 session_start();
 if (isset($_GET['item_number'])) {
-
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
-  $sql_rsrvtn = "SELECT * FROM tbl_passenger_reservation";
-  $stmt = $con->prepare($sql_rsrvtn);
-  $stmt->execute();
-  $stmt->store_result();
-  $stmt->fetch();
-  $num = $stmt->num_rows();
-  if($num > 0) {
-      $stmt->close();
-      error_reporting(E_ALL);
-      ini_set('display_errors', 1);
       $sql_em = "SELECT 
-                  tbl_pp.gross_income,
-                  tbl_pr.ship_name,
-                  tbl_pr.expiration,
-                  tbl_pr.reservation_number,
-                  tbl_pr.passenger_name,
-                  tbl_pr.location_from,
-                  tbl_pr.location_to,
-                  tbl_pr.depart_date,
-                  tbl_pr.depart_time,
-                  tbl_pr.accomodation,
-                  tbl_pr.reservation_date,
-                  tbl_pr.status,
-                  tbl_acctyp.accomodation_name,
-                  tbl_acctyp.seat_type,
-                  tbl_acctyp.aircon,
-                  tbl_acctyp.price,
-                  tbl_sched.location_from,
-                  tbl_sched.location_to,
-                  tbl_sched.depart_date,
-                  tbl_sched.depart_time,
-                  tbl_sched.port_from,
-                  tbl_sched.port_to
-                  FROM tbl_passenger_reservation tbl_pr
-                  JOIN tbl_psnger_pymnt tbl_pp 
-                  JOIN tbl_ship_schedule tbl_sched
-                  JOIN  tbl_ship_has_accomodation_type tbl_acctyp ON tbl_sched.id = tbl_acctyp.id
-                  WHERE tbl_pr.reservation_number=? AND tbl_pr.status = NULL";
+       tbl_pass_reserv.reservation_number,
+                        tbl_pass_reserv.ship_name,
+                        tbl_pass_reserv.passenger_name,
+                        tbl_pass_reserv.location_from,
+                        tbl_pass_reserv.location_to,
+                        tbl_pass_reserv.depart_date,
+                        tbl_pass_reserv.depart_time,
+                        tbl_pass_reserv.accomodation,
+                        tbl_pass_reserv.reservation_date,
+                        tbl_pass_reserv.expiration,
+                        tbl_pass_reserv.status,
+                        tbl_sd.ship_logo,
+                        tbl_sd.ship_name,
+                        tbl_sched.location_from,
+                        tbl_sched.location_to,
+                        tbl_sched.depart_date,
+                        tbl_sched.depart_time,
+                        tbl_acctyp.accomodation_name,
+                        tbl_acctyp.seat_type,
+                        tbl_acctyp.aircon,
+                        tbl_sched.port_from,
+                        tbl_sched.port_to,
+                        tbl_acctyp.price,
+                        tbl_tcket.tckt_promo,
+                        tbl_tcket.tckt_stats,
+                        tbl_tcket.tckt_dscnt,
+                        tbl_tcket.tckt_owner,
+                        tbl_tcket.tckt_price
+     from tbl_passenger_reservation tbl_pass_reserv
+     JOIN tbl_ship_detail tbl_sd ON tbl_pass_reserv.ship_name = tbl_sd.ship_name
+     JOIN tbl_ship_schedule tbl_sched ON tbl_sd.id = tbl_sched.id
+     JOIN tbl_ship_has_accomodation_type tbl_acctyp ON tbl_sched.id = tbl_acctyp.id
+     JOIN tbl_tckt tbl_tcket ON tbl_sd.ship_name = tbl_tcket.tckt_owner
+     WHERE tbl_pass_reserv.reservation_number=? LIMIT 1";
+     
       $s = $con->prepare($sql_em);
       $s->bind_param('s',$_GET['item_number']);
       $s->execute();
-      $result = $s->get_result();
-      $row = $result->fetch_array();
-     
-
-
-     
+      $result2 = $s->get_result();
+      while ($row = $result2->fetch_array()){
 ?>
 
 <!DOCTYPE html>
@@ -188,9 +181,8 @@ if (isset($_GET['item_number'])) {
 <div class='ticket__body'>
   <section class='ticket__section'>
     <h3>Your Tickets</h3>
-    <p><?php $row['accomodation_name']; ?></p>
-    <p>Block 406   Row Q   Seats 34-35</p>
-    <p>Your seats are together</p>
+    <p><?php echo $row['accomodation'] ?? 'Accomodation'; ?></p>
+    <p><?php echo $row['seat_type'] ?? 'No seats'?></p>
   </section>
   <section class='ticket__section'>
     <h3>Delivery Address</h3>
@@ -212,8 +204,8 @@ if (isset($_GET['item_number'])) {
 </html>
 
 <?php  
+      }
 }else{
   echo "2";
 }
-
-} ?>
+ ?>
